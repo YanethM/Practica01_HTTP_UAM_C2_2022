9f7a30a5fd4682d3a30b6fe2c86680d96c65f3bf
YanethM <yaneth.mejiar@autonoma.edu.co>
2022-03-30 07:36:00 -0500

    Using try-catch in async-await routes

--- src/routes/superhero_v2.router.js
+++ src/routes/superhero_v2.router.js
@@ -13,43 +13,53 @@ const service = new SuperheroService();
 404: Not found */
 
 superheroV2Router.post('/superhero', async (req, res) => {
-  const superheroV2 = superheroModel(req.body);
-  await service
-    .createSuperhero(superheroV2)
-    .then((data) => res.status(201).json(data))
-    .catch((err) => res.status(404).json({ message: err }));
+  try {
+    const superheroV2 = superheroModel(req.body);
+    const data = await service.createSuperhero(superheroV2);
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
 });
 
 superheroV2Router.get('/', async (req, res) => {
-  await service
-    .listSuperheroes()
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(404).json({ message: err }));
+  try {
+    const data = await service.listSuperheroes();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
 });
 
 superheroV2Router.get('/:superheroId', async (req, res) => {
-  const { superheroId } = req.params;
-  await service
-    .showSuperhero(superheroId)
-    .then((data) => res.status(302).json(data))
-    .catch((err) => res.status(404).json({ message: err }));
+  try {
+    const { superheroId } = req.params;
+    const data = await service.showSuperhero(superheroId);
+    res.status(302).json(data);
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
 });
 
 superheroV2Router.put('/:superheroId', async (req, res) => {
-  const { superheroId } = req.params;
-  const { superhero, realname, superpower } = req.body;
-  await service
-    .editSuperhero(superheroId, superhero, realname, superpower)
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(304).json({ message: err }));
+  try {
+    const { superheroId } = req.params;
+    const { superhero, realname, superpower } = req.body;
+    const data = await service.editSuperhero(superheroId, superhero, realname, superpower);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(304).json({ message: error });
+  }
 });
 
 superheroV2Router.delete('/:superheroId', async (req, res) => {
-  const { superheroId } = req.params;
-  await service
-    .removeSuperhero(superheroId)
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(404).json({ message: err }));
+  try {
+    const { superheroId } = req.params;
+    const data = await service.removeSuperhero(superheroId);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
 });
 
 module.exports = superheroV2Router;
